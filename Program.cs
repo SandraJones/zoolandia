a6a6f5381699da20f001ff532b75e8b8cc8235a6
Sandy Jones <sandynashv@gmail.com>
2016-07-18 17:01:01 -0500

    program.cs sentences printout

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace zoolandia
             donkey.Name = "Georgio";
             string response = donkey.Eat(4);
             Console.WriteLine(donkey.Name + " says " + response);
+            //is this just to rename Georgio or create a new instance of a donkey and name it Maury?
             EquusAsinus Maury = new EquusAsinus("Georgio");
             Console.WriteLine(Maury.Welcome("Maury"));
             Console.WriteLine(Maury.Name.ToUpper());
@@ -30,6 +31,10 @@ namespace zoolandia
             seaLion.Name = "Suzy";
             Console.WriteLine("What is Suzy's weight?");
             string response2 = Console.ReadLine();
+            //question why can't I do int response2?
+
+            Console.WriteLine("Suzy weighs  "  + response2 + " pounds.");
+            Console.WriteLine("Jimbo is " +  response3  +  " inches tall.");
     
             
 

fc53a20f8de96177286eca0935a6b8e71ae3237b
Sandy Jones <sandynashv@gmail.com>
2016-07-30 16:59:20 -0500

    finished habitats

---
+++ Species/IForestHabitat.cs
@@ -0,0 +1,12 @@
+using System;
+using zoolandia;
+using zoolandia.Species;
+
+namespace zoolandia.IForestHabitat
+{
+    interface IForestHabitat
+    {
+        bool livesInTrees();
+        string feeding();
+    }
+}
